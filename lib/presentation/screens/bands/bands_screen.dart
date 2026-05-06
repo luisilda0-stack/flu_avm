deddb871fdc2a9112fda7bfbd622472bab5b3f09
Luisa Tovar <luisilda0@gmail.com>
2026-05-06 18:42:51 +0200

    luitomen: Ventana de diálogo

--- lib/presentation/screens/bands/bands_screen.dart
+++ lib/presentation/screens/bands/bands_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flu_avm/config/config.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class BandsScreen extends StatelessWidget {
   const BandsScreen({super.key});
@@ -16,6 +18,11 @@ class BandsScreen extends StatelessWidget {
           return _bandTile(bands[i]);
         },
       ),
+      floatingActionButton: FloatingActionButton(
+        elevation: 1,
+        onPressed: () => addereNovmBan(context),
+        child: Icon(Icons.add),
+      ),
     );
   }
 
@@ -28,4 +35,43 @@ class BandsScreen extends StatelessWidget {
       trailing: Text('${ band.numerusVotum }', style: TextStyle(fontSize: 20),),
     );
   }
+}
+
+addereNovmBan(BuildContext context) {
+  final TextEditingController textumController = TextEditingController();
+
+  showCupertinoDialog(
+    context: context, 
+    builder: (BuildContext context) => CupertinoAlertDialog(
+      title: const Text('New band name'),
+      content: CupertinoTextField(
+        controller: textumController,
+        style: TextStyle(
+          color: Theme.of(context).brightness == Brightness.dark 
+            ? Colors.white 
+            : Colors.black
+        )
+      ),
+      actions: [
+        CupertinoDialogAction(
+          isDefaultAction: true,
+          child: const Text('Add'),
+          onPressed: () {
+            addereBandACollectione(context, textumController.text);
+          }
+        ),
+        CupertinoDialogAction(
+          isDestructiveAction: true,
+          child: const Text('Close'),
+          onPressed: () => context.pop()
+        ),
+      ],
+    )
+  );
+}
+
+addereBandACollectione(BuildContext context, String nomen) {
+  print(nomen);
+  context.pop();
+  bands.add(Band(id: DateTime.now().toString(), nomen: nomen, numerusVotum: 0));
 }
