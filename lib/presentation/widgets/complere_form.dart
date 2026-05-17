0d088b677c2025ddbf2454e4484bd8010dec3a0e
Luisa Tovar <luisilda0@gmail.com>
2026-05-17 22:40:39 +0200

    luitomen: formulario del mapa

--- lib/presentation/widgets/complere_form.dart
+++ lib/presentation/widgets/complere_form.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class ComplereForm extends StatelessWidget {
   const ComplereForm({super.key});
 
-  @override
+ @override
   Widget build(BuildContext context) {
     return ConstrainedBox(
       constraints: BoxConstraints(maxWidth: 200),
